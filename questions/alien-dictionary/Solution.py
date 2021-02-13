d4a22a21828cc9bf33a9e49ec19abf184b6050a1
franklingu <franklingujunchao@gmail.com>
2021-02-13 16:09:22 +0800

    fix: alien dictionary order with invalid cases
    Change-Id: I1c922a1da671b24d25a2bfe554b76cc405f8a3bb

--- questions/alien-dictionary/Solution.py
+++ questions/alien-dictionary/Solution.py
@@ -1,11 +1,36 @@
 """
+There is a new alien language that uses the English alphabet. However, the order among letters are unknown to you.
 
-None
+You are given a list of strings words from the dictionary, where words are sorted lexicographically by the rules of this new language.
+
+Derive the order of letters in this language, and return it. If the given input is invalid, return "". If there are multiple valid solutions, return any of them.
+
+Example 1:
+
+Input: words = ["wrt","wrf","er","ett","rftt"]
+Output: "wertf"
+Example 2:
+
+Input: words = ["z","x"]
+Output: "zx"
+Example 3:
+
+Input: words = ["z","x","z"]
+Output: ""
+Explanation: The order is invalid, so return "".
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] consists of only lowercase English letters.
 """
+
+import collections
 
 
 class Solution:
-    def alienOrder(self, words: List[str]) -> str:
+    def alienOrder(self, words):
         pre, suc = collections.defaultdict(set), collections.defaultdict(set)
         for pair in zip(words, words[1:]):
             for a, b in zip(*pair):
@@ -13,7 +38,10 @@ class Solution:
                     suc[a].add(b)
                     pre[b].add(a)
                     break
-        chars = set(''.join(words))
+            # invalid input, return empty string
+            if len(pair[0]) > len(pair[1]):
+                return ''
+        chars = set(suc).union(set(pre))
         free = chars - set(pre)
         order = ''
         while free:
