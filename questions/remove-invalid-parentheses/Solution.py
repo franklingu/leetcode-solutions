52a6109e6cb88dd3cbf4ba913e5af10deca5658d
franklingu <franklingujunchao@gmail.com>
2021-02-21 13:31:08 +0800

    fix: improve the run time complexity for removeInvalidParen
    Change-Id: I3353c60091d5bae882c05d3fc30c9d477652c0af

--- questions/remove-invalid-parentheses/Solution.py
+++ questions/remove-invalid-parentheses/Solution.py
@@ -20,55 +20,49 @@ Output: [""]
 """
 
 
-class Solution(object):
-    def removeInvalidParentheses(self, s):
-        """
-        :type s: str
-        :rtype: List[str]
-        """
-        def extra_parentheses(s):
-            cnt = 0
-            closing, opening = 0, 0
-            for c in s:
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        def get_extra(s):
+            closing = 0
+            stk = []
+            for i, c in enumerate(s):
                 if c == '(':
-                    cnt += 1
+                    stk.append(i)
                 elif c == ')':
-                    cnt -= 1
-                if cnt < 0:
-                    cnt = 0
-                    closing += 1
-            cnt = 0
-            for c in reversed(s):
-                if c == ')':
-                    cnt += 1
-                elif c == '(':
-                    cnt -= 1
-                if cnt < 0:
-                    cnt = 0
-                    opening += 1
-            return opening, closing
-        
-        def generate_valid(curr, index, opening, closing, ret):
-            if opening == 0 and closing == 0:
-                co, cc = extra_parentheses(curr)
-                if co == 0 and cc == 0:
-                    ret.add(''.join(curr))
+                    if not stk:
+                        closing += 1
+                    else:
+                        stk.pop()
+            return len(stk), closing
+            
+        def generate_valid(s, i, op, cc, opening, closing, ret):
+            if i >= len(s):
+                if opening != 0 or closing != 0:
+                    return
+                op, cc = get_extra(s)
+                if op == 0 and cc == 0:
+                    ret.add(''.join(s))
                 return
-            if index >= len(curr):
+            c = s[i]
+            if c != '(' and c != ')':
+                generate_valid(s, i + 1, op, cc, opening, closing, ret)
                 return
-            ch = curr[index]
-            if ch == '(' and opening > 0:
-                curr[index] = ''
-                generate_valid(curr, index + 1, opening - 1, closing, ret)
-                curr[index] = '('
-            elif ch == ')' and closing > 0:
-                curr[index] = ''
-                generate_valid(curr, index + 1, opening, closing - 1, ret)
-                curr[index] = ')'
-            generate_valid(curr, index + 1, opening, closing, ret)
+            elif c == '(':
+                if opening > 0:
+                    s[i] = ''
+                    generate_valid(s, i + 1, op, cc, opening - 1, closing, ret)
+                    s[i] = c
+                generate_valid(s, i + 1, op + 1, cc, opening, closing, ret)
+            elif c == ')':
+                if closing > 0:
+                    s[i] = ''
+                    generate_valid(s, i + 1, op, cc, opening, closing - 1, ret)
+                    s[i] = c
+                if op > cc:
+                    generate_valid(s, i + 1, op, cc + 1, opening, closing, ret)
+            
         
-        curr = list(s)
-        opening, closing = extra_parentheses(curr)
+        opening, closing = get_extra(s)
         ret = set()
-        generate_valid(curr, 0, opening, closing, ret)
+        generate_valid(list(s), 0, 0, 0, opening, closing, ret)
         return list(ret)
